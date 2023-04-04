50c0ee0291fb6c88e34c79498313d74a55fa3657
Hamza Rauf <hamzamian1@gmail.com>
2023-04-05 02:30:44 +0500

    Meme photos added

--- src/component/Meme.js
+++ src/component/Meme.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React ,{useState} from "react";
 import "./Meme.css";
+import memeData from "../memeData";
 
 const Meme = () => {
+  const [memeImage, setMemeImage]= useState("https://i.imgflip.com/4acd7j.png");
+  function getMemeImage(){
+    const memesArray = memeData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    setMemeImage(memesArray[randomNumber].url)
+  }
   return (
-    <main>
-      <form className="form">
+    <main>  
+      <div className="form">
         <input type="text" placeholder="Shut Up" className="form_input"/>
         <input type="text" placeholder="And Take My Money" className="form_input"/>
-        <button className="button">Get a new meme image 🖼️</button>
-      </form>
+        <button onClick={getMemeImage}  className="button">Get a new meme image 🖼️</button>
+      </div>
+      <img className="meme_image" src={memeImage} alt="Meme"/>
     </main>
   );
 };
